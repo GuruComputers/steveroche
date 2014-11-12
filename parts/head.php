3ce4becc69d7b8227c0e6a0c74e92ff1839baeef
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-11-12 21:18:32 +0000

    Add Slideshow

--- parts/head.php
+++ parts/head.php
@@ -6,4 +6,7 @@
 	<link rel="stylesheet" href="css/main.css">
 	<link rel="stylesheet" href="css/responsive.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
+    <script src="../js/galleria-1.4.2.min.js"></script>
+    <script src="../js/plugins/facebook/galleria.facebook.js"></script>
 </head>
