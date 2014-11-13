e0a2f4a72a738cd56f9da7345be56821d09dc15a
Rev Neil Wakely <neil@gurucomputers.co.uk>
2014-11-13 12:54:32 +0000

    index.php
    cleanup code add comments

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 	echo "<!DOCTYPE html>"."\n";
 	echo "<html>"."\n";
 	/*
-	Head section of HTML code echoed output_add_rewrite_var*/
+	Head section of HTML code echoed out
+	*/
 	echo "<head>"."\n";
 	echo "<meta charset=\"utf-8\">"."\n";
 	echo "<title>S.M. Roche</title>"."\n";
@@ -20,54 +21,88 @@
 	*/
 	echo "<body>"."\n";
 	echo "<div id=\"wrapper\">"."\n"; // Main wrapper declared for page
-	/*
-	HEADER coded by Neil Wakely
-	*/
+	/***********************************************************************************
+	HEADER 
+	Section : 1
+	Coded by : Neil Wakely
+	***********************************************************************************/
 	echo "<div id=\"header\">"."\n";
 	echo "<header>"."\n";
-	echo "<a href=\"index.php\">"."\n";
-	echo "<img src=\"img/logo.png\" id=\"logo\" alt=\"S.M. Roche Logo\">"."\n";
+	echo "<a href=\"index.php\">";
+	echo "<img src=\"img/logo.png\" id=\"logo\" alt=\"S.M. Roche Logo\">";
 	echo "</a>"."\n";
 	echo "<nav>"."\n";
 	echo "<ul>"."\n";
 	if ($page == "home") {
-		echo '<li><a href="index.php" class="selected">Home</a></li>';
+		echo "<li><a href=\"index.php\" class=\"selected\">Home</a></li>"."\n";
 	} else {
-		echo '<li><a href="index.php">Home</a></li>';
+		echo "<li><a href=\"index.php\">Home</a></li>"."\n";
 	}
 	
 	if ($page == "testimonials") {
-		echo '<li><a href="testimonials.php" class="selected">Testimonials</a></li>';
+		echo "<li><a href=\"testimonials.php\" class=\"selected\">Testimonials</a></li>"."\n";
 	} else {
-		echo '<li><a href="testimonials.php">Testimonials</a></li>';
+		echo "<li><a href=\"testimonials.php\">Testimonials</a></li>"."\n";
 	}
 	
 	if ($page == "gallery") {
-		echo '<li><a href="gallery.php" class="selected">Gallery</a></li>';
+		echo "<li><a href=\"gallery.php\" class=\"selected\">Gallery</a></li>"."\n";
 	} else {
-		echo '<li><a href="gallery.php">Gallery</a></li>';
+		echo "<li><a href=\"gallery.php\">Gallery</a></li>"."\n";
 	}
 
 	if ($page == "About") {
-		echo '<li><a href="about.php" class="selected">About</a></li>';
+		echo "<li><a href=\"about.php\" class=\"selected\">About</a></li>"."\n";
 	} else {
-		echo '<li><a href="about.php">About</a></li>';
+		echo "<li><a href=\"about.php\">About</a></li>"."\n";
 	}
 
 	if ($page == "contact") {
-		echo '<li><a href="contct.php" class="selected">Contact</a></li>';
+		echo "<li><a href=\"contct.php\" class=\"selected\">Contact</a></li>"."\n";
 	} else {
-		echo '<li><a href="contact.php">Contact</a></li>';
+		echo "<li><a href=\"contact.php\">Contact</a></li>"."\n";
 	}
 	echo "</ul>"."\n";
 	echo "</nav>"."\n";
 	echo "</header>"."\n";
-	echo "</div>"."\n";
-	include("parts/2.html"); // Slideshow
-	include("parts/3.html"); // Content menu
-	include("parts/4.html"); // News & Customer Testimonials
-	include("parts/5.php"); // Footer
-	include("parts/6.php"); // Copyright
+	echo "</div>"."\n";	
+	/***********************************************************************************
+	SLIDESHOW
+	Section : 2
+	Coded by : Dave Wakely
+	***********************************************************************************/
+	include("2.php"); // Slideshow
+	/***********************************************************************************
+	CONTENT MENU 
+	Section : 3
+	Coded by : Dave Wakely
+	***********************************************************************************/
+	include("3.php"); // Content menu
+	/***********************************************************************************
+	NEWS & CUSTOMER TESTIMONIALS 
+	Section : 4
+	Coded by : Dave Wakely
+	***********************************************************************************/
+	include("4.php"); // News & Customer Testimonials
+	include("5.php"); // Footer
+	/***********************************************************************************
+	Copyright
+	Section : 6
+	Coded by : Neil Wakely
+	***********************************************************************************/
+	echo "<div id=\"copyright\">"."\n";
+	$start=2014;
+	$current=date("Y");
+	$cy="";
+	if ($current=$start) {
+		$cy=$start;
+	}
+	if ($current>$start) {
+		$cy=$start."-".$current;
+	}
+	echo "Designed & Hosted by <a href=\"http://gurucomputers.co.uk\" alt=\"guru computers ltd website\" id=\"guru\" target=\"_blank\">Guru Computers Ltd.</a>"."<br />"."\n";
+	echo "&copy"." ".$cy." S.M. Roche"."\n";
+	echo "</div>"."\n"."\n";
 	/*
 	End of normal HTML code to close of page echoed out
 	*/
