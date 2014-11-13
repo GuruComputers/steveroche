65ca056f4a068edd2b71d165a46ce9ad66c09758
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-11-13 12:55:13 +0000

    PHP Facebook Photo Integration

--- parts/2.php
+++ parts/2.php
@@ -1,14 +1,22 @@
+<?php
 /**********************************
       Facebook Image Slider
          By Dave Wakely
 **********************************/
 
-<?php
+
 	// Set the Page ID
 	$FBid = '468113209957335';
 
 	// Get The Contents of the Facebook Page
-	$FBpage = file_get_contents(‘https://graph.facebook.com/’.$FBid.’/albums’);
+	$FBpage = file_get_contents(‘https:\/\/graph.facebook.com/’.$FBid.’/albums’);
 
 	// Interpret Data with JSON
-	
+	$photoData = json_decode($FBpage);
+
+	// Get Album List
+	foreach ($photoData->data as $data)
+	{
+		echo '<li>'.$data.'</li>';
+	}
+?>
