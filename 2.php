40102d1357705abe20aeae1192b64de35aeee614
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-11-13 15:38:27 +0000

    Update Facebook Slider

--- 2.php
+++ 2.php
@@ -11,38 +11,20 @@
 	
 	$FBid = "468113209957335"; // Set the Page ID
 
-	// Get The Contents of the Facebook Page
-	$FBpage = file_get_contents("https://graph.facebook.com/".$FBid."/albums?fields=id,name,description,link,cover_photo,count");
+	$fbPhotos_link = "http://graph.facebook.com/497225217046134/photos?fields=source";
+	$fbPhotos = file_get_contents($fbPhotos_link);
 
-	// Interpret Data with JSON
-	$photoData = json_decode($FBpage, true, 512, JSON_BIGINT_AS_STRING);
+	$obj = json_decode($fbPhotos, true);
 
-	// Collate Albums
-	$album_process = count($photoData['data']);
+	$photo_count = count($obj["data"]);
 
-	// Process Albums
-	for($x=0; $x<$album_process; $x++){
+	for($x=0; $x<$photo_count; $x++){
 
-		$id = $photoData['data'][$x]['id'];
-		$name = $photoData['data'][$x]['name'];
-		$description = $photoData['data'][$x]['description'];
-		$link = $photoData['data'][$x]['link'];
-		$cover_photo = $photoData['data'][$x]['cover_photo'];
-		$count = $photoData['data'][$x]['count'];
+		$source = $obj["data"][$x]["source"];
 
-		// Exclude Albums we don't want displayed on the website
-		if(
-			$name != "Profile Pictures" &&
-			$name != "Cover Photos" &&
-			$name != "Timeline Photos"
-		){
-			$show_pictures_link = "photos.php?album_id={$id}&album_name={$name}";
-
-			echo "<a href = '{$show_pictures_link}'>";
-			echo "<img class='img-responsive' src='http://graph.facebook.com/{$cover_photo}/picture' alt=''>";
-			echo "</a>";
-		}
-
+		echo "<a href=\"{$source}\" data-gallery>";
+			echo "<img src=\"".$source."\">";
+		echo "</a>";
 	}
 
 	echo "</div>";
