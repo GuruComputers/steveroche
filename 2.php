7b041442f35b8eb70f23594a038ac47be69b169d
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-11-13 13:10:28 +0000

    PHP Facebook Slider Bug Fix #4

--- 2.php
+++ 2.php
@@ -6,7 +6,7 @@
 
 
 	// Set the Page ID
-	$FBid = '468113209957335';
+	$FBid = "468113209957335";
 
 	// Get The Contents of the Facebook Page
 	$FBpage = file_get_contents("https://graph.facebook.com/".$FBid."/albums");
@@ -14,9 +14,12 @@
 	// Interpret Data with JSON
 	$photoData = json_decode($FBpage);
 
+	//Temp Test Echo
+	echo $FBid;
+
 	// Get Album List
 	foreach ( $photoData->data as $data )
 	{
-		echo ‘<li>’.$data->name.’</li>’;
+		echo "<li>".$data->name."</li>";
 	}
 ?>
