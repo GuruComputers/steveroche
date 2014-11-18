6915e0c82653e0e9b1e5c1142403b3fe13d0a8c3
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-11-18 15:12:26 +0000

    Add Customer Testimonials Section

--- 4.php
+++ 4.php
@@ -10,7 +10,6 @@
         <div style="float: left; width: 21px; height:10px;"></div>
         <div style="float: left; width: 380px;">
         	<img src="img/speech.gif" class="img_l" alt="" />
-            <h3>“Customer Testimonials”</h3>
             <div style="clear: both;"></div>
             <span class="span-content">Space here to showcase Customer Testimonials</span><a href="testimonials.php" class="more2"></a>
         </div>
