856f7d7b2a34421dcf1e10b1bbfa619ae45d5975
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-11-18 13:41:21 +0000

    Further Style Adjustments for Section 3

--- 3.php
+++ 3.php
@@ -13,27 +13,34 @@ echo "<span class=\"span-describe\">I work on my own so I can therefore keep my 
 echo "<span class=\"span-describe\">Please take a look at my photographs on my page and if you are happy with what you see give me a ring 07592262165.</span>"."\n";
 echo "<div style=\"float: left; width: 32px; height:10px;\"></div>"."\n";
 echo "<div style=\"float: left; width: 658px;\">"."\n";
+echo "<h2>Services</h2>"."\n";
+echo "<div style=\"height:20px\"></div>"."\n";
+echo "<div>"."\n";
 
 // Box for Plumbing Services
 echo "<div class=\"box\">"."\n";
 echo "<span class=\"span-content\">Plumbing Services</span>"."\n";
-echo "<div style=\"height:20px\"></div>";
+echo "<div style=\"height:20px\"></div>"."\n";
 echo "<img src=\"img/plumbing.png\" alt=\"Plumbing Services\">"."\n";
 echo "</div>"."\n";
 
 // Box for Roofing Services
 echo "<div class=\"box\">"."\n";
 echo "<span class=\"span-content\">Roofing Services</span>"."\n";
+echo "<div style=\"height:20px\"></div>"."\n";
 echo "<img src=\"img/plumbing.png\" alt=\"Roofing Services\">"."\n";
 echo "</div>"."\n";
 
 // Box for Associated Works Services
 echo "<div class=\"box\">"."\n";
 echo "<span class=\"span-content\">Associated Works</span>"."\n";
+echo "<div style=\"height:20px\"></div>"."\n";
 echo "<img src=\"img/plumbing.png\" alt=\"Associated Works\">"."\n";
 echo "</div>"."\n";
+echo "<div style=\"clear: both;\"></div>"."\n";
 
 // End of Main Content Div
+echo "</div>"."\n";
 echo "</div>"."\n";
 echo "</div>"."\n";
 echo "<br />"."\n";
