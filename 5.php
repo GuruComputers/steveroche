fe9af6fa2a0654e2278bf2235aadf287e4c18021
Rev Neil Wakely <neil@gurucomputers.co.uk>
2014-11-18 13:02:07 +0000

    css tweaks

--- 5.php
+++ 5.php
@@ -3,7 +3,7 @@
 	echo "<center><h3><p id=\"contact\">Contact Information</p></h3></center><br />"."\n";
 	echo "<table align=\"center\" width=\"100%\" height=\"100px\">"."\n";
 	echo "<tr>"."\n";
-	echo "<td bgcolor=\"white\" align=\"center\" valign=\"top\"><a href=\"tel:07871828709\"><img src=\"img/phone.png\"></a></td>"."\n"; 
+	echo "<td bgcolor=\"white\" align=\"center\" valign=\"top\"><a href=\"tel:07871828709\">Telephone</a></td>"."\n"; 
 	echo "<td bgcolor=\"white\" align=\"center\" valign=\"top\"><a href=\"mailto:steve@sm-roche.co.uk\">Email Address</a></td>"."\n"; 
 	echo "<td bgcolor=\"white\" align=\"center\" valign=\"top\"><a href=\"https:\/\/www.facebook.com\/pages\/S-M-Roche\/468113209957335\">S.M. Roche Facebook Page</a></td>"."\n"; 
 	echo "<td bgcolor=\"white\" align=\"center\" valign=\"top\"><a href=\"http://twitter.com/intent/tweet?screen_name=sm_roche\">Send us a tweet</a></td>"."\n"; 
