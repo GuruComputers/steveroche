d7329327ef80d40a94618d20e4ce7a5a20531567
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-11-18 14:57:56 +0000

    Further Style Adjustments

--- 3.php
+++ 3.php
@@ -1,37 +1,36 @@
 <div id="content">
 
-    <div style="height:25px"></div>
     <div>
 
         <div style="float:left; width: 250px; padding-left: 5px">
         	<h2>About Us</h2>
-            <div style="height:20px"></div>
+            <div style="height:10px"></div>
             <span class="span-describe">Although I have over 20 years experience as a flat roofer, I am also a qualified plumber and can do general house maintenance.
 
 <p>I can repair or replace Guttering, Soffits, Fascias, Fencing and fit Velux Windows. </p>
 <p>As a professional tradesman I can receieve generous discounts from many builders merchants and pass them onto you!</p>
-<a href="about.php">Read More</a></span></div>
-        <div style="float: left; width: 32px; height:10px;"></div>
+<p align="center"><a href="about.php">Read More</a></span></p></div>
+        <div style="float: left; width: 32px;"></div>
         <div style="float: left; width: 658px;">
 
             	<h2>Services</h2>
             	<div style="height:20px"></div>
                 <div>
 
-                	<div class="box">
+                	<div class="box" align="center">
                     	<span class="span-content">Plumbing Services</span>
                         <div style="height:10px"></div>
                   <img src="img/plumbing.png" alt="" /></div>
                   <div style="height:10px; width: 23px; float:left"></div>
 
-                    <div class="box">
+                    <div class="box" align="center">
                     	<span class="span-content">Roofing Services</span> <br />
                     	<div style="height:10px"></div>
                         <img src="img/roofing.png" alt="" />
                   </div>
 
                   <div style="height:10px; width: 23px; float:left"></div>
-                    <div class="box">
+                    <div class="box" align="center">
                     	<span class="span-content">Associated Works</span>
                     	<div style="height:10px"></div>
                         <img src="img/aworks.png" alt="" />
--- 4.php
+++ 4.php
@@ -6,12 +6,13 @@
     <div style="float: left; width: 250px;">
         <h2>News</h2>
         <div style="height:20px"></div>
-        <span class="span_cont">News about yor company can go here. New services, products etc etc</span></div>
+        <span class="span-content">News Section</span></div>
         <div style="float: left; width: 21px; height:10px;"></div>
         <div style="float: left; width: 380px;">
+        <img src="img/speech.gif" class="img_l" alt="" />
             <h3>“Customer Testimonials”</h3>
             <div style="clear: both;"></div>
-            <span class="span_cont">Space here to showcase Customer Testimonials</span><a href="#" class="more2"></a>
+            <span class="span-content">Space here to showcase Customer Testimonials</span><a href="testimonials.php" class="more2"></a>
         </div>
         <div style="clear: both;"></div>
     </div>
