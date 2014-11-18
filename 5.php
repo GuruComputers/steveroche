6c7a1f2cd4985b5f2ad57f35418fff90f371adbc
Rev Neil Wakely <neil@gurucomputers.co.uk>
2014-11-18 19:22:28 +0000

    5.php hover buttons created and fixed

--- 5.php
+++ 5.php
@@ -6,16 +6,16 @@
 				<a href="tel:07871828709"><div class="phone"></div></a>
 			</td>
 			<td>
-				<a href="mailto:steve@sm-roche.co.uk"><img src="./img/email.png"></a>
+				<a href="mailto:steve@sm-roche.co.uk"><div class="email"></div></a>
 			</td>
 			<td>
-				<a href="https://www.facebook.com/pages/S-M-Roche/468113209957335"><img src="./img/facebook.png"></a>
+				<a href="https://www.facebook.com/pages/S-M-Roche/468113209957335"><div class="facebook"></div></a>
 			</td> 
 			<td>
-				<a href="http://twitter.com/intent/tweet?screen_name=sm_roche"><img src="./img/tweet.png"></a>
+				<a href="http://twitter.com/intent/tweet?screen_name=sm_roche"><div class="tweet"></div></a>
 			</td>
 			<td>
-				<a href="http://twitter.com/sm_roche"><img src="./img/twitter.png"></a>
+				<a href="http://twitter.com/sm_roche"><div class="twitter"></div></a>
 			</td>
 		</tr>
 	</table>
