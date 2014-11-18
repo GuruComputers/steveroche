c94c89501a28a0129c4c8233a6ba06ddd5a264d5
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-11-18 13:20:35 +0000

    Add News Section Skeleton File

--- 3.php
+++ 3.php
@@ -1,35 +1,35 @@
 <?php
 
 // Main Content Div
-echo "<div id=\"contact\">";
+echo "<div id=\"contact\">"."\n"; 
 
 // Floating Div for About Us Section
-echo "<div style=\"float:left; width: 250px;\">";
-echo "<h2>About Us</h2>";
-echo "<span class=\"span-describe\">Although i have over 20 years experience as a flat roofer, i am also a qualified plumber and can do general house maintenance.</span>";
-echo "<span class=\"span-describe\">I can repair or replace Guttering, Soffits, Fascias, fencing and fit Velux windows.</span>";
-echo "<span class=\"span-describe\">I work on my own so I can therefore keep my costs low. As a professional tradesman I can receieve generous discounts from many builders merchants and pass them onto you!!</span>"; 
-echo "<span class=\"span-describe\">Please take a look at my photographs on my page and if you are happy with what you see give me a ring 07592262165.</span>";
-echo "</div>";
+echo "<div style=\"float:left; width: 250px;\">"."\n";
+echo "<h2>About Us</h2>"."\n";
+echo "<span class=\"span-describe\">Although i have over 20 years experience as a flat roofer, i am also a qualified plumber and can do general house maintenance.</span>"."\n";
+echo "<span class=\"span-describe\">I can repair or replace Guttering, Soffits, Fascias, fencing and fit Velux windows.</span>"."\n";
+echo "<span class=\"span-describe\">I work on my own so I can therefore keep my costs low. As a professional tradesman I can receieve generous discounts from many builders merchants and pass them onto you!!</span>"."\n"; 
+echo "<span class=\"span-describe\">Please take a look at my photographs on my page and if you are happy with what you see give me a ring 07592262165.</span>"."\n";
+echo "</div>"."\n";
 
 // Box for Plumbing Services
-echo "<div class=\"box\">";
-echo "<span class=\"span-content\">Plumbing Services</span>";
-echo "<img src=\"img/plumbing.png\" alt=\"Plumbing Services\">";
-echo "</div>";
+echo "<div class=\"box\">"."\n";
+echo "<span class=\"span-content\">Plumbing Services</span>"."\n";
+echo "<img src=\"img/plumbing.png\" alt=\"Plumbing Services\">"."\n";
+echo "</div>"."\n";
 
 // Box for Roofing Services
-echo "<div class=\"box\">";
-echo "<span class=\"span-content\">Roofing Services</span>";
-echo "<img src=\"img/plumbing.png\" alt=\"Roofing Services\">";
-echo "</div>";
+echo "<div class=\"box\">"."\n";
+echo "<span class=\"span-content\">Roofing Services</span>"."\n";
+echo "<img src=\"img/plumbing.png\" alt=\"Roofing Services\">"."\n";
+echo "</div>"."\n";
 
 // Box for Associated Works Services
-echo "<div class=\"box\">";
-echo "<span class=\"span-content\">Associated Works</span>";
-echo "<img src=\"img/plumbing.png\" alt=\"Associated Works\">";
-echo "</div>";
+echo "<div class=\"box\">"."\n";
+echo "<span class=\"span-content\">Associated Works</span>"."\n";
+echo "<img src=\"img/plumbing.png\" alt=\"Associated Works\">"."\n";
+echo "</div>"."\n";
 
 // End of Main Content Div
-echo "</div>";
-echo "<br />";
+echo "</div>"."\n";
+echo "<br />"."\n";
---
+++ 4.php
@@ -0,0 +1,10 @@
+<?php
+
+// Main Content Div
+echo "<div id=\"news\">"."\n"; 
+
+// Temp Data
+echo "<h2>Section 4</h2>";
+
+// End of Main Content Div
+echo "</div>"."\n";
