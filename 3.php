fd2c03ca7fa982a39eabbcd206143641d258155c
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-11-18 14:07:11 +0000

    Rewrite of Section 4

--- 3.php
+++ 3.php
@@ -44,23 +44,6 @@ Please take a look at my photographs on my page and if you are happy with what y
             </div>
             <div style="clear: both;"></div>
         </div>
-
-        <div style="height:50px"></div>
-        <div>
-          <div style="float: left; width: 32px; height:10px;"></div>
-            <div style="float: left; width: 250px;">
-            	<h2>News</h2>
-           	  	<div style="height:20px"></div>
-                <span class="span_cont">News about yor company can go here. New services, products etc etc</span></div>
-            <div style="float: left; width: 21px; height:10px;"></div>
-            <div style="float: left; width: 380px;">
-            	<img src="images/img_fish.gif" class="img_l" alt="" />
-            	<h3>“Customer Testimonials”</h3>
-                <div style="clear: both;"></div>
-                <span class="span_cont">Space here to showcase Customer Testimonials</span><a href="#" class="more2"></a>
-            </div>
-            <div style="clear: both;"></div>
-        </div>
         <div style="height:50px"></div>        
         
     </div>
--- 4.php
+++ 4.php
@@ -1,10 +1,17 @@
-<?php
-
-// Main Content Div
-echo "<div id=\"news\">"."\n"; 
-
-// Temp Data
-echo "<h2>Section 4</h2>";
-
-// End of Main Content Div
-echo "</div>"."\n";
+<div style="height:50px"></div>
+<div>
+    <div style="float: left; width: 32px; height:10px;"></div>
+    <div style="float: left; width: 250px;">
+        <h2>News</h2>
+        <div style="height:20px"></div>
+        <span class="span_cont">News about yor company can go here. New services, products etc etc</span></div>
+        <div style="float: left; width: 21px; height:10px;"></div>
+        <div style="float: left; width: 380px;">
+            <img src="images/img_fish.gif" class="img_l" alt="" />
+            <h3>“Customer Testimonials”</h3>
+            <div style="clear: both;"></div>
+            <span class="span_cont">Space here to showcase Customer Testimonials</span><a href="#" class="more2"></a>
+        </div>
+        <div style="clear: both;"></div>
+    </div>
+</div>
