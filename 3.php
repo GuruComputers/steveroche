cbb12c3955cd7e8070374784e691437cbac7b602
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-11-18 12:41:39 +0000

    Add Content Section Skeleton
    Skeleton File for Content Section Created

---
+++ 3.php
@@ -0,0 +1,30 @@
+<?php
+
+// Main Content Div
+echo "<div id=\"content\">";
+
+// Floating Div for About Us Section
+echo "<div style=\"float:left; width: 250px;\">";
+echo "<h2>About Us</h2>";
+echo "</div>";
+
+// Box for Plumbing Services
+echo "<div class=\"box\">";
+echo "<span class=\"span-content\">Plumbing Services</span>";
+echo "<img src=\"img/plumbing.png\" alt=\"Plumbing Services\">";
+echo "</div>";
+
+// Box for Roofing Services
+echo "<div class=\"box\">";
+echo "<span class=\"span-content\">Roofing Services</span>";
+echo "<img src=\"img/plumbing.png\" alt=\"Roofing Services\">";
+echo "</div>";
+
+// Box for Associated Works Services
+echo "<div class=\"box\">";
+echo "<span class=\"span-content\">Associated Works</span>";
+echo "<img src=\"img/plumbing.png\" alt=\"Associated Works\">";
+echo "</div>";
+
+// End of Main Content Div
+echo "</div>";
