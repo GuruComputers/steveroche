916c28406b50bc929e54c43a90f3df16373b91c5
Rev Neil Wakely <neil@gurucomputers.co.uk>
2014-11-18 12:55:02 +0000

    Updating contact section

--- 5.php
+++ 5.php
@@ -3,12 +3,11 @@
 	echo "<center><h3><p id=\"contact\">Contact Information</p></h3></center><br />"."\n";
 	echo "<table align=\"center\" width=\"100%\" height=\"100px\">"."\n";
 	echo "<tr>"."\n";
-	echo "<td bgcolor=\"white\" align=\"center\" valign=\"top\">Postal Address</td>"."\n";
-	echo "<td bgcolor=\"white\" align=\"center\" valign=\"top\"><a href=\"tel:\">Phone Number</a></td>"."\n"; 
-	echo "<td bgcolor=\"white\" align=\"center\" valign=\"top\"><a href=\"mailto:\">Email Address</a></td>"."\n"; 
+	echo "<td bgcolor=\"white\" align=\"center\" valign=\"top\"><a href=\"tel:07871828709\"><img src=\"img/phone.png\"></a></td>"."\n"; 
+	echo "<td bgcolor=\"white\" align=\"center\" valign=\"top\"><a href=\"mailto:steve@sm-roche.co.uk\">Email Address</a></td>"."\n"; 
 	echo "<td bgcolor=\"white\" align=\"center\" valign=\"top\"><a href=\"https:\/\/www.facebook.com\/pages\/S-M-Roche\/468113209957335\">S.M. Roche Facebook Page</a></td>"."\n"; 
-	echo "<td bgcolor=\"white\" align=\"center\" valign=\"top\"><a href=\"http://twitter.com/intent/tweet?screen_name=\">Send us a tweet</a></td>"."\n"; 
-	echo "<td bgcolor=\"white\" align=\"center\" valign=\"top\"><a href=\"http://twitter.com/\">Twitter Page</a></td>"."\n"; 
+	echo "<td bgcolor=\"white\" align=\"center\" valign=\"top\"><a href=\"http://twitter.com/intent/tweet?screen_name=sm_roche\">Send us a tweet</a></td>"."\n"; 
+	echo "<td bgcolor=\"white\" align=\"center\" valign=\"top\"><a href=\"http://twitter.com/sm_roche\">Twitter Page</a></td>"."\n"; 
 	echo "</tr>"."\n"; 
 	echo "</table>"."\n";
 	echo "</div>"."\n";
