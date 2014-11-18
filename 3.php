a743ca7e226f1323c0cfc5de26bd270966704641
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-11-18 14:03:08 +0000

    Rewrite of Section 3

--- 3.php
+++ 3.php
@@ -1,53 +1,66 @@
-<?php
+<div id="content">
 
-// Main Services Div
-echo "<div id=\"content\">"."\n"; 
+    <div style="height:25px"></div>
+    <div>
 
-// Floating Div for About Us Section
-echo "<div style=\"float:left; width: 250px;\">"."\n";
-echo "<h2>About Us</h2>"."\n";
-echo "<div style=\"height:20px\"></div>"."\n";
-echo "<span class=\"span-describe\">Although i have over 20 years experience as a flat roofer, i am also a qualified plumber and can do general house maintenance.</span>"."\n";
-echo "<span class=\"span-describe\">I can repair or replace Guttering, Soffits, Fascias, fencing and fit Velux windows.</span>"."\n";
-echo "<span class=\"span-describe\">I work on my own so I can therefore keep my costs low. As a professional tradesman I can receieve generous discounts from many builders merchants and pass them onto you!!</span>"."\n"; 
-echo "<span class=\"span-describe\">Please take a look at my photographs on my page and if you are happy with what you see give me a ring 07592262165.</span>"."\n";
-echo "</div">."\n";
+        <div style="float:left; width: 250px;">
+        	<h2>About Us</h2>
+            <div style="height:20px"></div>
+            <span class="span_cont">Although I have over 20 years experience as a flat roofer, I am also a qualified plumber and can do general house maintenance.
 
-// Setting Up Divs for Services
-echo "<div style=\"float: left; width: 32px; height:10px;\"></div>"."\n";
-echo "<div style=\"float: left; width: 658px;\">"."\n";
-echo "<h2>Services</h2>"."\n";
-echo "<div style=\"height:20px\"></div>"."\n";
-echo "<div>"."\n";
+I can repair or replace Guttering, Soffits, Fascias, fencing and fit Velux windows. 
+I work on my own so I can therefore keep my costs low. 
+As a professional tradesman iI can receieve generous discounts from many builders merchants and pass them onto you!! 
+Please take a look at my photographs on my page and if you are happy with what you see give me a ring 07592262165.</span></div>
+        <div style="float: left; width: 32px; height:10px;"></div>
+        <div style="float: left; width: 658px;">
 
-// Box for Plumbing Services
-echo "<div class=\"box\">"."\n";
-echo "<span class=\"span-content\">Plumbing Services</span>"."\n";
-echo "<div style=\"height:10px\"></div>"."\n";
-echo "<img src=\"img/plumbing.png\" alt=\"Plumbing Services\">"."\n";
-echo "</div>"."\n";
-echo "<div style=\"height:10px; width: 23px; float:left\"></div>"."\n";
+            	<h2>Services</h2>
+            	<div style="height:20px"></div>
+                <div>
 
-// Box for Roofing Services
-echo "<div class=\"box\">"."\n";
-echo "<span class=\"span-content\">Roofing Services</span>"."\n";
-echo "<div style=\"height:20px\"></div>"."\n";
-echo "<img src=\"img/plumbing.png\" alt=\"Roofing Services\">"."\n";
-echo "</div>"."\n";
-echo "<div style=\"height:10px; width: 23px; float:left\"></div>"."\n";
+                	<div class="box">
+                    	<span class="span_cont">Plumbing Services</span>
+                        <div style="height:10px"></div>
+                  <img src="images/tap.png" alt="" /></div>
+                  <div style="height:10px; width: 23px; float:left"></div>
 
-// Box for Associated Works Services
-echo "<div class=\"box\">"."\n";
-echo "<span class=\"span-content\">Associated Works</span>"."\n";
-echo "<div style=\"height:20px\"></div>"."\n";
-echo "<img src=\"img/plumbing.png\" alt=\"Associated Works\">"."\n";
-echo "</div>"."\n";
-echo "<div style=\"clear: both;\"></div>"."\n";
+                    <div class="box">
+                    	<span class="span_cont">Roofing Services</span> <br />
+                    	<div style="height:10px"></div>
+                        <img src="images/img2.jpg" alt="" />
+                  </div>
 
-// End of Services Div
-echo "</div>"."\n";
-echo "</div>"."\n";
-echo "<div style=\"clear: both;\"></div>"."\n";
-echo "</div>"."\n";
+                  <div style="height:10px; width: 23px; float:left"></div>
+                    <div class="box">
+                    	<span class="span_cont">Associated Works</span>
+                    	<div style="height:10px"></div>
+                        <img src="images/img3.jpg" alt="" />
+                    </div>
 
-?>
+                    <div style="clear: both;"></div>
+                    
+                </div>
+            </div>
+            <div style="clear: both;"></div>
+        </div>
+
+        <div style="height:50px"></div>
+        <div>
+          <div style="float: left; width: 32px; height:10px;"></div>
+            <div style="float: left; width: 250px;">
+            	<h2>News</h2>
+           	  	<div style="height:20px"></div>
+                <span class="span_cont">News about yor company can go here. New services, products etc etc</span></div>
+            <div style="float: left; width: 21px; height:10px;"></div>
+            <div style="float: left; width: 380px;">
+            	<img src="images/img_fish.gif" class="img_l" alt="" />
+            	<h3>“Customer Testimonials”</h3>
+                <div style="clear: both;"></div>
+                <span class="span_cont">Space here to showcase Customer Testimonials</span><a href="#" class="more2"></a>
+            </div>
+            <div style="clear: both;"></div>
+        </div>
+        <div style="height:50px"></div>        
+        
+    </div>
