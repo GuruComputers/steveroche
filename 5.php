65a5da78c39e0f559f1e381e7a961f5b2055b152
Rev Neil Wakely <neil@gurucomputers.co.uk>
2014-11-18 18:24:31 +0000

    replaced images

--- 5.php
+++ 5.php
@@ -3,19 +3,19 @@
 	<table width="100%" height="100px">
 		<tr>
 			<td>
-				<a href="tel:07871828709">Telephone</a>
+				<a href="tel:07871828709"><img src="./img/phone.png"></a>
 			</td>
 			<td>
-				<a href="mailto:steve@sm-roche.co.uk">Email Address</a>
+				<a href="mailto:steve@sm-roche.co.uk"><img src="./img/email.png"></a>
 			</td>
 			<td>
-				<a href="https://www.facebook.com/pages/S-M-Roche/468113209957335">S.M. Roche Facebook Page</a>
+				<a href="https://www.facebook.com/pages/S-M-Roche/468113209957335"><img src="./img/facebook.png"></a>
 			</td> 
 			<td>
-				<a href="http://twitter.com/intent/tweet?screen_name=sm_roche">Send us a tweet</a>
+				<a href="http://twitter.com/intent/tweet?screen_name=sm_roche"><img src="./img/tweet.png"></a>
 			</td>
 			<td>
-				<a href="http://twitter.com/sm_roche">Twitter Page</a>
+				<a href="http://twitter.com/sm_roche"><img src="./img/twitter.png"></a>
 			</td>
 		</tr>
 	</table>
