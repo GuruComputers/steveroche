3716488e078d737075b8b872c3c91e0a93937c85
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-11-18 16:16:45 +0000

    Add blank.png

--- 4.php
+++ 4.php
@@ -4,9 +4,20 @@
 <div>
     <div style="float: left; width: 32px; height:10px;"></div>
     <div style="float: left; width: 250px;">
-        <h2>News</h2>
+        <a class="twitter-timeline"  
+        href="https://twitter.com/hashtag/news" 
+        data-widget-id="534734596181483520"
+        data-screen-name="sm_roche"
+        data-show-replies="false"
+        data-tweet-limit="3"
+        data-chrome="nofooter transparent">
+        	<h2>News</h2>
+        </a>
         <div style="height:20px"></div>
-        <span class="span-content">News Section</span></div>
+        <span class="span-content">
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+          
+        </span></div>
         <div style="float: left; width: 21px; height:10px;"></div>
         <div style="float: left; width: 380px;">
         	<img src="img/speech.gif" class="img_l" alt="" />
