eeb1fc12850802948f034f2aa16b7080c0f94320
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-11-18 13:57:01 +0000

    Further Styling to Section 3

--- 3.php
+++ 3.php
@@ -1,6 +1,6 @@
 <?php
 
-// Main Content Div
+// Main Services Div
 echo "<div id=\"content\">"."\n"; 
 
 // Floating Div for About Us Section
@@ -11,6 +11,9 @@ echo "<span class=\"span-describe\">Although i have over 20 years experience as 
 echo "<span class=\"span-describe\">I can repair or replace Guttering, Soffits, Fascias, fencing and fit Velux windows.</span>"."\n";
 echo "<span class=\"span-describe\">I work on my own so I can therefore keep my costs low. As a professional tradesman I can receieve generous discounts from many builders merchants and pass them onto you!!</span>"."\n"; 
 echo "<span class=\"span-describe\">Please take a look at my photographs on my page and if you are happy with what you see give me a ring 07592262165.</span>"."\n";
+echo "</div">."\n";
+
+// Setting Up Divs for Services
 echo "<div style=\"float: left; width: 32px; height:10px;\"></div>"."\n";
 echo "<div style=\"float: left; width: 658px;\">"."\n";
 echo "<h2>Services</h2>"."\n";
@@ -20,9 +23,10 @@ echo "<div>"."\n";
 // Box for Plumbing Services
 echo "<div class=\"box\">"."\n";
 echo "<span class=\"span-content\">Plumbing Services</span>"."\n";
-echo "<div style=\"height:20px\"></div>"."\n";
+echo "<div style=\"height:10px\"></div>"."\n";
 echo "<img src=\"img/plumbing.png\" alt=\"Plumbing Services\">"."\n";
 echo "</div>"."\n";
+echo "<div style=\"height:10px; width: 23px; float:left\"></div>"."\n";
 
 // Box for Roofing Services
 echo "<div class=\"box\">"."\n";
@@ -30,6 +34,7 @@ echo "<span class=\"span-content\">Roofing Services</span>"."\n";
 echo "<div style=\"height:20px\"></div>"."\n";
 echo "<img src=\"img/plumbing.png\" alt=\"Roofing Services\">"."\n";
 echo "</div>"."\n";
+echo "<div style=\"height:10px; width: 23px; float:left\"></div>"."\n";
 
 // Box for Associated Works Services
 echo "<div class=\"box\">"."\n";
@@ -39,10 +44,10 @@ echo "<img src=\"img/plumbing.png\" alt=\"Associated Works\">"."\n";
 echo "</div>"."\n";
 echo "<div style=\"clear: both;\"></div>"."\n";
 
-// End of Main Content Div
+// End of Services Div
 echo "</div>"."\n";
 echo "</div>"."\n";
+echo "<div style=\"clear: both;\"></div>"."\n";
 echo "</div>"."\n";
-echo "<br />"."\n";
 
 ?>
