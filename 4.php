3127bb6c9c437e58576ce2bf0a8c16dc0cd8048c
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-11-18 15:01:28 +0000

    Typo Fix in Section 3

--- 4.php
+++ 4.php
@@ -9,14 +9,14 @@
         <span class="span-content">News Section</span></div>
         <div style="float: left; width: 21px; height:10px;"></div>
         <div style="float: left; width: 380px;">
-        <img src="img/speech.gif" class="img_l" alt="" />
+        	<img src="img/speech.gif" class="img_l" alt="" />
             <h3>“Customer Testimonials”</h3>
             <div style="clear: both;"></div>
             <span class="span-content">Space here to showcase Customer Testimonials</span><a href="testimonials.php" class="more2"></a>
         </div>
         <div style="clear: both;"></div>
     </div>
-</div>
 <div style="height:25px"></div>
 <div class="line"></div>
 <div style="height:25px"></div>
+</div>
