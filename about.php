c07ae632586e9e4637921884d7933f60ea8243a5
Rev Neil Wakely <neil@gurucomputers.co.uk>
2014-11-21 13:11:08 +0000

    Menu tweaked

--- about.php
+++ about.php
@@ -68,11 +68,17 @@ Coded by : Neil Wakely
 <nav>
 <ul>
 <?php
-	$page="index";
+	$page="about";
 	$menu = array("Index", "Testimonials", "Gallery", "About", "Contact");
 	foreach ($menu as $item) {
+		if (strtolower($item) == "home"){
+			$item = "Index";
+		}
    	 	echo '<li><a href="'.strtolower($item).'.php"';
    	 	if (strtolower($item) == $page){
+   	 		echo ' class="selected"';
+   	 	}
+   	 	if (strtolower($item) == "index"){
    	 		echo ' class="selected"';
    	 	}
    	 	if (strtolower($item) == "index"){
@@ -89,49 +95,14 @@ Coded by : Neil Wakely
 
 
 <!--
-Facebook Image Slider
+About Content
 Section : 2
 Coded by : Dave Wakely
 -->
-<div id ="slideshow">
-<?php
-	$FBid = "468113209957335"; // Set the Page ID
-	$fbPhotos_link = "http://graph.facebook.com/497225217046134/photos?fields=source";
-	$fbPhotos = file_get_contents($fbPhotos_link);
-	$obj = json_decode($fbPhotos, true);
-	$photo_count = count($obj["data"]);
-	echo "<div class=carousel>"."\n";
-	echo "<ul class=panes>"."\n";
-	for($x=0; $x<$photo_count; $x++){
-		echo "<li>";
-		$source = $obj["data"][$x]["source"];
-		echo "<img src=\"".$source."\">"."\n";
-		echo "</li>"."\n";
-	}
-?>
-</ul>
+<div id ="about_content">
+<!-- Page code goes here -->
 </div>
 <img src="img/hr.png" class="hr"> <!-- middle hr rule -->
-
-
-<!--
-CONTENT MENU 
-Section : 3
-Coded by : Dave Wakely
--->
-<?php
-	include("3.php"); // <!-- Content menu -->
-?>
-
-
-<!--
-NEWS & CUSTOMER TESTIMONIALS 
-Section : 4
-Coded by : Dave Wakely
--->
-<?php
-	include("4.php"); // <!-- News & Customer Testimonials -->
-?>
 
 
 <!--
