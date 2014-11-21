3ecf439752a8176c79388f381c8223533f23f874
Rev Neil Wakely <neil@gurucomputers.co.uk>
2014-11-21 12:50:27 +0000

    Small cleanup of html code

--- index.php
+++ index.php
@@ -7,8 +7,6 @@ Website : Managed
 -->
 
 
-
-
 <!--
 Normal HTML code echoed out
 Coded by : Neil Wakely
@@ -143,28 +141,29 @@ FOOTER
 Section : 5
 Coded by : Neil Wakely
 -->
-	<div id ="footer">
-	<h3>Contact Information</h3>
-	<table width="100%" height="100px">
-		<tr>
-			<td>
-				<a href="tel:07871828709"><div class="phone"></div></a>
-			</td>
-			<td>
-				<a href="mailto:steve@sm-roche.co.uk"><div class="email"></div></a>
-			</td>
-			<td>
-				<a href="https://www.facebook.com/pages/S-M-Roche/468113209957335" target="_blank"><div class="facebook"></div></a>
-			</td> 
-			<td>
-				<a href="http://twitter.com/intent/tweet?screen_name=sm_roche"><div class="tweet"></div></a>
-			</td>
-			<td>
-				<a href="http://twitter.com/sm_roche" target="_blank"><div class="twitter"></div></a>
-			</td>
-		</tr>
-	</table>
+<div id ="footer">
+<h3>Contact Information</h3>
+<table width="100%" height="100px">
+<tr>
+<td>
+	<a href="tel:07871828709"><div class="phone"></div></a>
+</td>
+<td>
+	<a href="mailto:steve@sm-roche.co.uk"><div class="email"></div></a>
+</td>
+<td>
+	<a href="https://www.facebook.com/pages/S-M-Roche/468113209957335" target="_blank"><div class="facebook"></div></a>
+</td> 
+<td>
+	<a href="http://twitter.com/intent/tweet?screen_name=sm_roche"><div class="tweet"></div></a>
+</td>
+<td>
+	<a href="http://twitter.com/sm_roche" target="_blank"><div class="twitter"></div></a>
+</td>
+</tr>
+</table>
 </div>
+
 
 <!--
 Copyright
