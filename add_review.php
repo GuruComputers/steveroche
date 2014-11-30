cd9d487ae93465d1af693d6119b2a788827712c3
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-11-30 15:10:12 +0000

    Update Testimonials

--- add_review.php
+++ add_review.php
@@ -1,29 +1,30 @@
 <?php
 
+	error_reporting(E_ALL);
+
 	$host="localhost"; // Host name 
-	$username="smroche"; // Mysql username 
-	$password="JDZfLtEBJFhRCNRB"; // Mysql password 
+	$username="sm-roche"; // Mysql username 
+	$password="customer31337"; // Mysql password 
 	$db_name="testimonials"; // Database name 
-	$tbl_name="approved"; // Table name 
 
 	// Connect to server and select database.
-	mysql_connect("$host", "$username", "$password")or die("cannot connect server "); 
-	mysql_select_db("$db_name")or die("cannot select DB");
+	$conn = new mysqli("$host", "$username", "$password", "$db_name");
+
+	// Check connection
+	if ($conn->connect_error) {
+    	die("Connection failed: " . $conn->connect_error);
+	} 
 
 	$datetime=date("y-m-d h:i:s"); //date time
 
-	$sql="INSERT INTO $tbl_name(review_name, service_type, rating, review, status, datetime)VALUES('$reviewer_name', '$service_type', '$rating', '$review', '$status', '$datetime')";
-	$result=mysql_query($sql);
+	$sql = "INSERT INTO approved (review_name, service_type, rating, review, status, datetime) VALUES ('$reviewer_name', '$service_type', '$rating', '$review', '$status', '$datetime')";
 
-	//check if query successful 
-	if($result){
-	echo "Successful";
-	echo "<BR>";
-
-	else {
-	echo "ERROR";
+	if ($conn->query($sql) === TRUE) {
+    	echo "New record created successfully";
+	} else {
+    	echo "Error: " . $sql . "<br>" . $conn->error;
 	}
 	
-	mysql_close();
+	$conn->close();
 
 ?>
