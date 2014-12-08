1f779cf9c6bb026356352438041c8096770dabc7
Rev Neil Wakely <neil@gurucomputers.co.uk>
2014-12-08 16:46:03 +0000

    recoding done

--- temp.php
+++ temp.php
@@ -1,22 +1,24 @@
 <img src="./img/hr.png" class="hr"> <!-- middle hr rule -->
 <a name="contact"></a>
-<div id ="footer">
-	<h3>Contact Information</h3>
+<center>
+	<div id ="footer">
+		<h3>Contact Information</h3>
 
-	<div class="box_content_l" align="center">
-		<a href="tel:07871828709"><br/>Telephone<br/></a>
-	</div>
+		<div class="box_content_l" align="center">
+			<a href="tel:07871828709"><br/>Telephone<br/>(07871) 828709</a>
+		</div>
 
-	<div class="box_content_c" align="center">
-		<a href="mailto:steve@sm-roche.co.uk"><br/>email<br/>steve@sm-roche.co.uk</a>
-	</div>
-		
-	<div class="box_content_c" align="center">
-		<a href="https://www.facebook.com/pages/S-M-Roche/468113209957335" target="_blank">Facebook<br>www.facebook.com/<br/>pages/S-M-Roche/<br/>468113209957335</a>
-	</div> 
+		<div class="box_content_c" align="center">
+			<a href="mailto:steve@sm-roche.co.uk"><br/>email<br/>steve@sm-roche.co.uk</a>
+		</div>
+			
+		<div class="box_content_c" align="center">
+			<a href="https://www.facebook.com/pages/S-M-Roche/468113209957335" target="_blank">Facebook<br>www.facebook.com/<br/>pages/S-M-Roche/<br/>468113209957335</a>
+		</div> 
 
-	<div class="box_content_r" align="center">
-		<a href="http://twitter.com/intent/tweet?screen_name=sm_roche">Send Us A Tweet<br/><br/></a>
-		<a href="http://twitter.com/sm_roche" target="_blank">Twitter Page<br/>http://twitter.com/sm_roche</a>
+		<div class="box_content_r" align="center">
+			<a href="http://twitter.com/intent/tweet?screen_name=sm_roche">Send Us A Tweet<br/><br/></a>
+			<a href="http://twitter.com/sm_roche" target="_blank">Twitter Page<br/>http://twitter.com/sm_roche</a>
+		</div>
 	</div>
-</div>
+</center>
