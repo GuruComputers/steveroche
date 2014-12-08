b024d4d2a7453e1dae925f993a471674c90c0b72
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-12-08 15:33:17 +0000

    Add About Page

--- about.php
+++ about.php
@@ -10,7 +10,20 @@ Section : 2
 Coded by : 
 -->
 <div id ="about_content">
-<!-- Page code goes here -->
+	<section>
+    	<img src="./img/me.jpg" alt="Photograph of Steve Roche" class="profilePic">
+        <h3>About</h3>
+        <p>Hi, I'm Steve Roche! Although i have over 20 years experience as a flat roofer, i am also a qualified plumber and can do general house maintenance.</p>
+        <p>I can repair or replace Guttering, Soffits, Fascias, fencing and fit Velux windows. I work on my own so i can therefore keep my costs low. As a professional tradesman i can receieve generous discounts from many builders merchants and pass them onto you!! Please take a look at my photographs on my page and if you are happy with what you see give me a ring 07592262165.</p>
+        <h3>My Qualifications</h3>
+        <ul>
+        	<li>H.S.E - C S C S card</li>
+			<li>Water Regs and Hot Water Systems</li>
+			<li>City and Guilds Level 2 Plumbing</li>
+			<li>Working at Heights</li>
+			<li>Asbestos Awareness</li>
+		</ul>
+    </section>
 </div>
 
 
